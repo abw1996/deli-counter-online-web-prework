bf0c15320ee7b17a9baac017eccd48b39aca9e50
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-07 19:15:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,15 +4,16 @@ def line(deli)
     puts "The line is currently empty."
   else
     start = 0 
-    name = deli.fetch(start)
+    
     place = (start + 1).to_s
     length = deli.length
     phrase = "The line is currently: "
-    length.times do 
-      phrase = phrase + place + ". " + name
+    length.times do
+      name = deli.fetch(start)
+      phrase = phrase + place + ". " + name " "
       start += 1
     end
-    puts phrase
+    puts phrase.chop
   end
 end
 
