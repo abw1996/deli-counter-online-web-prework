51e74bef6ea353bd6c4b6a0502bbe82117de90ad
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-07 19:10:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def line(deli)
+  if deli == [] 
+    puts "The line is currently empty."
+  else
+    start = 0 
+    name = deli.fetch(start)
+    place = (start + 1).to_s
+    length = deli.length
+    phrase = "The line is currently: "
+    length.times do 
+      phrase = phrase + place + ". " + name
+      start += 1
+    end
+    return phrase
+  end
+end
+
