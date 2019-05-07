7c47928b0728ca68d96e9ed2048fb1c3adf7e1b1
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-07 19:46:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -31,5 +31,6 @@ def take_a_number(deli, name)
 end
 #======================================================================
 def now_serving(deli)
-  
+  if deli == []
+    puts "There is nobody waiting to be served!"
 end 
